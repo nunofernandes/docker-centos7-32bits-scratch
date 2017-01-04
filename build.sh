61684a8687161f9f920bc90c04806fb76a162bc2
Nuno Fernandes <npf@eurotux.com>
2017-01-04 22:24:42 +0000

    Set img2tar new path. See #1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /ami-creator/ami_creator/ami_creator.py -c /centos7.ks
-/sig-cloud-instance-build/docker/img2tar.sh centos7-*.img
+/sig-cloud-instance-build/docker/deprecated/img2tar.sh centos7-*.img
